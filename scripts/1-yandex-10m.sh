b09cbbb7b948fd3c980ec1368870aa25b23293c7
Philipp Hager <philipp.konstantin.hager@gmail.com>
2025-08-03 09:40:07 +0200

    Update 1-yandex-10m.sh

--- scripts/1-yandex-10m.sh
+++ scripts/1-yandex-10m.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
-python main.py -m \
-  experiment=1-yandex-10m/1/ \
-  model=GCTR,DCTR,RCTR,CM,PBM,UBM,DBN,SDBN,DCM,CCM \
-  dataset=/ivi/ilps/datasets/yandex/relevance_prediction/YandexClicks.txt \
-  train_sessions=[0,10_000_000] \
-  val_sessions=[10_000_000,15_000_000] \
-  test_sessions=[15_000_000,20_000_000] \
-  eval_train_queries_only=True \
-  $@
+#python main.py -m \
+#  experiment=1-yandex-10m/1/ \
+#  model=GCTR,DCTR,RCTR,CM,PBM,UBM,DBN,SDBN,DCM,CCM \
+#  dataset=/ivi/ilps/datasets/yandex/relevance_prediction/YandexClicks.txt \
+#  train_sessions=[0,10_000_000] \
+#  val_sessions=[10_000_000,15_000_000] \
+#  test_sessions=[15_000_000,20_000_000] \
+#  eval_train_queries_only=True \
+#  $@
 
 python main.py -m \
   experiment=1-yandex-10m/2/ \
