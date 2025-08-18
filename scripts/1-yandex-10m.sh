cc759e8166c725e9835d248fbf9624de2f538b11
Philipp Hager <philipp.konstantin.hager@gmail.com>
2025-08-18 14:43:12 +0200

    WIP 1M trial run

--- scripts/1-yandex-10m.sh
+++ scripts/1-yandex-10m.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
-#python main.py -m \
-#  experiment=1-yandex-10m/1/ \
-#  model=GCTR,DCTR,RCTR,CM,PBM,UBM,DBN,SDBN,DCM,CCM \
-#  dataset=/ivi/ilps/datasets/yandex/relevance_prediction/YandexClicks.txt \
-#  train_sessions=[0,10_000_000] \
-#  val_sessions=[10_000_000,15_000_000] \
-#  test_sessions=[15_000_000,20_000_000] \
-#  eval_train_queries_only=True \
-#  $@
+python main.py -m \
+  experiment=1-yandex-10m/1/ \
+  model=GCTR,DCTR,RCTR,CM,PBM,UBM,DBN,SDBN,DCM,CCM \
+  dataset=/ivi/ilps/datasets/yandex/relevance_prediction/YandexClicks.txt \
+  train_sessions=[0,1_000_000] \
+  val_sessions=[1_000_000,2_000_000] \
+  test_sessions=[2_000_000,3_000_000] \
+  min_train_sessions_per_eval_query=10 \
+  $@
 
 #python main.py -m \
 #  experiment=1-yandex-10m/2/ \
@@ -19,13 +19,13 @@
 #  test_sessions=[25_000_000,30_000_000] \
 #  eval_train_queries_only=True \
 #  $@
-
-python main.py -m \
-  experiment=1-yandex-10m/3/ \
-  model=GCTR,DCTR,RCTR,CM,PBM,UBM,DBN,SDBN,DCM,CCM \
-  dataset=/ivi/ilps/datasets/yandex/relevance_prediction/YandexClicks.txt \
-  train_sessions=[20_000_000,30_000_000] \
-  val_sessions=[30_000_000,35_000_000] \
-  test_sessions=[35_000_000,40_000_000] \
-  eval_train_queries_only=True \
-  $@
+#
+#python main.py -m \
+#  experiment=1-yandex-10m/3/ \
+#  model=GCTR,DCTR,RCTR,CM,PBM,UBM,DBN,SDBN,DCM,CCM \
+#  dataset=/ivi/ilps/datasets/yandex/relevance_prediction/YandexClicks.txt \
+#  train_sessions=[20_000_000,30_000_000] \
+#  val_sessions=[30_000_000,35_000_000] \
+#  test_sessions=[35_000_000,40_000_000] \
+#  eval_train_queries_only=True \
+#  $@
